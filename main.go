4afac381342bfee659ff34bad387162b87e11288
imrojony <imrojony@gmail.com>
2018-04-29 20:33:52 +0600

    chapter 5

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
+//chapter 4
+//package main
+//
+//import "fmt"
+//
+//func add(x,y int) int {
+//	return x+y
+//
+//}
+//func main()  {
+//	fmt.Println(add(13,23))
+//
+//}
+//chapter 5
 package main
 
 import "fmt"
 
-func add(x,y int) int {
-	return x+y
-	
+func add(x, y int) int {
+	return x + y
 }
-func main()  {
-	fmt.Println(add(13,23))
-	
+
+func main() {
+	fmt.Println(add(42, 13))
 }
