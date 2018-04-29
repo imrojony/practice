726b50f426da63f7ffa8c52564d27a7ce0b110bf
imrojony <imrojony@gmail.com>
2018-04-29 20:45:21 +0600

    chapter 7

--- main.go
+++ main.go
@@ -25,15 +25,29 @@
 //	fmt.Println(add(42, 13))
 //}
 //chapter 6
+//package main
+//
+//import "fmt"
+//
+//func swap(x, y string) (string, string) {
+//	return y, x
+//}
+//
+//func main() {
+//	a, b := swap("hello", "world")
+//	fmt.Println(a, b)
+//}
+//chapter 7
 package main
 
 import "fmt"
 
-func swap(x, y string) (string, string) {
-	return y, x
+func split(sum int) (x, y int) {
+x = sum * 4 / 9
+y = sum - x
+return
 }
 
 func main() {
-	a, b := swap("hello", "world")
-	fmt.Println(a, b)
+fmt.Println(split(17))
 }
